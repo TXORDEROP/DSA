dc0b3e465ec2e00d8df5fe5df517ba2a82fccc31
TXORDEROP <chipadeaditya001@gmail.com>
2025-07-29 09:28:56 +0530

    Dsa : Operators and Strings:

---
+++ src/operatorresss.java
@@ -0,0 +1,12 @@
+public class operatorresss {
+    public static void main(String[] args) {
+        int a=0;
+        int b = 10;
+
+//        a = b++;
+//        a=++b;
+//        a=--b;
+//        a=b--;
+        System.out.println(a + " " + " " + b);
+    }
+}
